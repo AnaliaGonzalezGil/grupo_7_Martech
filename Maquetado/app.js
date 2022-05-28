75e60c10036014b13e9cb11092ca96ae2603f4f2
panaze <panaze@outlook.com>
2022-05-28 17:03:46 -0500

    Agregue archivo login.html

--- Maquetado/app.js
+++ Maquetado/app.js
@@ -1,12 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-
-app.listen(3000, ()=>{
-    console.log('Servidor Funcionando');
+app.listen(3000, () => {
+  console.log("Servidor Funcionando");
 });
 
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/home.html");
 });
