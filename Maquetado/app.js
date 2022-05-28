45a0a63d13b275823ffffa89489d8f967a2d9fa5
klechmann <kev_tb@live.com.ar>
2022-05-28 20:45:56 -0300

    Ya todas las html indexadas al app.js

--- Maquetado/app.js
+++ Maquetado/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 app.use(express.static("public"));
+
 
 app.listen(3000, () => {
   console.log("Servidor Funcionando");
@@ -9,3 +11,10 @@ app.listen(3000, () => {
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/home.html");
 });
+app.get("/login", (req, res) => {
+    res.sendFile(__dirname + "/views/login.html");
+});
+app.get("/carrito", (req, res) => {
+    res.sendFile(__dirname + "/views/carrito.html");
+});
+
