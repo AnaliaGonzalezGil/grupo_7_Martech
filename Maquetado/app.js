200d55fd1b6b3af448ed3b648053f41a1f18db86
klechmann <kev_tb@live.com.ar>
2022-06-06 20:49:02 -0300

    Mas estilos del Home

--- Maquetado/app.js
+++ Maquetado/app.js
@@ -8,7 +8,7 @@ app.listen(3000, () => {
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/home.html");
+  res.sendFile(__dirname + "/views/index.html");
 });
 app.get("/login", (req, res) => {
   res.sendFile(__dirname + "/views/login.html");
@@ -19,4 +19,8 @@ app.get("/Detalle", (req, res) => {
 
 app.get("/register", (req, res) => {
   res.sendFile(__dirname + "/views/register.html");
+});
+
+app.get("/carrito", (req, res) => {
+  res.sendFile(__dirname + "/views/carrito.html");
 });
