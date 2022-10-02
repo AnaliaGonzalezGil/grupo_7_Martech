884f452de7682315068feb73017f0d7f9c53a247
jorgenassar <104800294+jorgenassar@users.noreply.github.com>
2022-10-02 01:08:20 -0500

    Add files via upload
    Primera parte a un 90%, falta segunda parte

---
+++ Maquetado/src/controllers/api/productsController.js
@@ -0,0 +1,21 @@
+const db = require("../../database/models");
+const sequelize = db.sequelize;
+const productos = db.Product;
+
+const productsController = {
+    todos: (req,res) => {
+        db.Product.findAll()
+                .then(products => {
+                    return res.status(200).json({meta:{status:200,
+                                        count: products.length,
+                                        url:'api/products'},
+                                    data: {products: products.map(function(products){
+                                    return products.id  + "," + products.nombreProducto + ","+ products.descripcion + "," +  '/api/products/detalle/' + products.id
+                                                                                    })
+                                        }}
+                                                )
+        })
+    }
+}
+
+module.exports = productsController;
